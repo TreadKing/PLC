b63754abd9d8b6502aeacf18b98d65342adc332d
TreadKing <devangpatel@Devangs-MBP.attlocal.net>
2020-05-03 19:58:32 -0400

    fixed question 5

--- Final/Final_5.java
+++ Final/Final_5.java
@@ -2,22 +2,26 @@
  * PLC final Question 5
  */
 
- import java.util.*;
- public class Final_5{
-     public static void main(String[] args){
+import java.util.*;
+public class Final_5{
+    public static void main(String[] args){
+       int j;
+       int i;
+       int n = 3;
+       int[][] x = {{1,2,3}, {0,0,0}, {23,51,74}};
 
-        int n = 3;
-        int[][] x = {{1,2,3}, {0,0,0}, {23,51,74}};
+       for(i = 0; i< n; i++){
+           for(j = 0; j <n; j++){
+               if(x[i][j] == 0)
+                   j++;
 
-        for(int i = 0; i<= n; i++){
-            for(int j = 0; j <=n; j++){
-                if(x[i][j] != 0){
-                    break;
-                else if(j == n)
-                    System.out.println("First all-zero row is: " + i);
-                }
-            }
-        }
+               else 
+                   break;
+           }
+       if(j == n)
+           System.out.println("Firat all zero row is at row: " + i);
+       }
+       
+    }
 
-     }
- }
+}
