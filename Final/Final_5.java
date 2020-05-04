625747b49fd3ad6e11d7b314d037ced20a6cdac6
TreadKing <devangpatel@Devangs-MBP.attlocal.net>
2020-05-03 20:22:26 -0400

    Actaully fixed Q5

--- Final/Final_5.java
+++ Final/Final_5.java
@@ -1,27 +1,31 @@
-/**Devang Patel
- * PLC final Question 5
- */
+//Devang Patel
+//PLC Final Question 5
 
-import java.util.*;
-public class Final_5{
-    public static void main(String[] args){
-       int j;
-       int i;
-       int n = 3;
-       int[][] x = {{1,2,3}, {0,0,0}, {23,51,74}};
+import java.util.*; 
+public class Final_5 {
+	public static void main(String[] args) {
+		
+		int i; int j; int n = 3; 
+		
+		int[][] x = {{1,2,3}, {0, 0, 0,},{10, 40, 49,}};
+		
 
-       for(i = 0; i< n; i++){
-           for(j = 0; j <n; j++){
-               if(x[i][j] == 0)
-                   j++;
-
-               else 
-                   break;
-           }
-       if(j == n)
-           System.out.println("Firat all zero row is at row: " + i);
-       }
-       
-    }
-
+		for( i = 0; i <= n; i++) {
+			for(j = 0; j < n;) {
+				if(x[i][j] == 0) 
+					j++;
+				
+				else 
+					break;
+				
+			}
+			if(j == n) {
+				System.out.print("Fist row with the all zeros: Row " + i);
+				break;
+			} 
+			else {}
+		
+		}
+		 
+	}
 }
